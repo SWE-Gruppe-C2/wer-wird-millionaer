5e4c449366b3a9c37628e5ebaad465274f994526
alvwin <alvwin167@gmail.com>
2022-11-19 15:53:29 +0100

    implemented suggestions

--- database/seeders/CSVDatabaseSeeder.php
+++ database/seeders/CSVDatabaseSeeder.php
@@ -6,6 +6,11 @@ class CSVDatabaseSeeder extends DatabaseSeeder
 {
 
     /**
+     * generates an associative array from a csv file, taking the first row of the csv as keys for the array
+     * key1,key2
+     * value1_1,value1_2
+     * value2_1,value2_2
+     * [0] => [[key1]=>[value1_1],[key2]=>[value1_2]],[1] => [[key1]=>[value2_1]...]
      * @param $path string to csv file
      * @return array csv contents in associative array
      */
