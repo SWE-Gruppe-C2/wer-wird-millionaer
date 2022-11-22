1c02a46cdfb524c8066563232e0088ef3d03e61e
kaifedin24 <kai.fedin@gmail.com>
2022-11-22 11:43:20 +0100

    Einbettung CSS File & Frontend game-end & leaderboard
    TImos CSS File wurde eingebettet und erstes Setup auf Basis seiner Templates.
    
    Noch scheinen die default CSS Einstellungen mit WWM CSS zu konkurieren

--- resources/views/leaderboard.blade.php
+++ resources/views/leaderboard.blade.php
@@ -1,6 +1,12 @@
 <x-app-layout>
-
-    <div>
+    <link href="{{ asset('style.css') }}" rel="stylesheet">
+    <div class="horizontal_bar">
+        <img src="img/mute.png" id="toggle_sound" alt="Musik einschalten">
+        <a href="login.html">
+            <img src="img/logout.png" id="logout" alt="Abmelden">
+        </a>
+    </div>
+    <div class="leaderboard-table">
         @php $games = App\Http\Controllers\GameController::index(); @endphp
         <table>
             <tr>
