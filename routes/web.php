2c3048c504cf0c4a73eaf44d637f75af4e96f94d
Elian <elian.drendel@gmail.com>
2022-11-25 16:27:04 +0100

    Main Menu Page
    Added css and js basic functionality.
    
    Added "main-menu" page with functionality for logout and the two buttons redirecting to template pages "play" and "highscores".
    
    Added route for "main-menu", and two routes for the templates pages.
    
    Added images.

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,16 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('main-menu', function() {
+    return view('main-menu');
+})->name('main-menu');
+
+Route::get('/play', function() {
+    return view('play');
+})->name('play');
+
+Route::get('/highscores', function() {
+    return view('highscores');
+})->name('highscores');
+
 require __DIR__.'/auth.php';
