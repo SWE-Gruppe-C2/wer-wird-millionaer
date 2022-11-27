8cf8dfa9649505c8061d7df98680cd53eab9503f
lina <95224518+linanilsu@users.noreply.github.com>
2022-11-27 16:05:10 +0100

    category-add.blade.php

---
+++ resources/views/category-add.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>category add</title>
+
+</head>
+<body>
+    <main>
+        <div class="horizontal_bar">
+            <form action="{{ route('back') }}" method="POST">
+                @csrf
+                <button type="submit">
+                    <img src="{{ asset('assets/img/back.png') }}" id="back" alt="Back">
+                </button>
+            </form>
+            <form action="{{ route('logout') }}" method="POST">
+                @csrf
+                <button type="submit">
+                    <img src="{{ asset('assets/img/logout.png') }}" id="logout" alt="Logout">
+                </button>
+            </form>
+        </div>
+        <div>
+            <h1>Kategorie Hinzufügen</h1>
+        </div>
+
+        <div class="">
+            <form action="" method="post">
+                <input type="text" id="category" name="category" placeholder="Category">
+                <input type="submit" value="Kategorie hinzufügen">
+            </form>
+        </div>
+    </main>
+</body>
+</html>
+
