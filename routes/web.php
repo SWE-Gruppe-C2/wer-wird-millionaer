08ab0287ac570927f1ef2fd3de75ef2dd39378ff
hannah <hannah.lehnen@gmx.de>
2022-11-27 17:29:44 +0100

    category-edit-overview.blade.php, category-edit-success.blade.php und category-edit.blade.php hinzugefügt

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,16 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/category-edit-overview',function(){
+    return view('category-edit-overview');
+})->name('category-edit-overview');
+
+Route::get('/category-edit',function(){
+    return view('category-edit');
+})->name('category-edit');
+
+Route::get('/category-edit-success',function(){
+    return view('category-edit-success');
+})->name('category-edit-success');
+
 require __DIR__.'/auth.php';
