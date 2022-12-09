e82dd4e8ebb14a4582778cfc9c1f162ff1b02701
kaifedin24 <kai.fedin@gmail.com>
2022-12-09 11:38:42 +0100

    init commit

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,6 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::resource();
+
 require __DIR__.'/auth.php';
