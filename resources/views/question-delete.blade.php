a473290d744ad2127d3fe55acdcd0dd40765f9bd
Luke <luke.vincent.braun@gmail.com>
2022-12-11 20:14:36 +0100

    ini question delete

---
+++ resources/views/question-delete.blade.php
@@ -0,0 +1,11 @@
+@extends(layout.app)
+
+
+<h1>Frage Löschen </h1>
+
+<form>
+
+    <input type="button" name="delete" value="delete">
+    <input type="button" name="abort" value="abort">
+</form>
+
