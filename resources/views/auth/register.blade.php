12c82f478f3473b82ef51f49d7dceefa981dcf1b
Luke <luke.vincent.braun@gmail.com>
2022-12-11 20:21:08 +0100

    start migrating to component layout

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -43,3 +43,41 @@
 </main>
 </body>
 </html>
+
+
+ /* switch to component layout
+
+<x-base-layout :tile="'Register'">
+    @section(navigation)
+    <nav>
+        <h1>Registrieren</h1>
+    </nav>
+    @endsection
+
+        <form method="POST" action="{{ route('register') }}">
+            @csrf
+            <!-- Username -->
+            <div>
+                <input type="text" id="name" name="name" placeholder="Benutzername" required autofocus>
+            </div>
+
+            <!-- Email -->
+            <div>
+                <input type="email" id="email" name="email" placeholder="E-Mail" required>
+            </div>
+
+            <!-- Passwort -->
+            <div>
+                <input type="password" id="password" name="password" placeholder="Neues Passwort eingeben" required>
+            </div>
+
+            <!-- Passwort bestätigen-->
+            <div>
+                <input type="password" id="password_confirmation" name="password_confirmation" placeholder="Passwort wiederholen" required>
+            </div>
+
+            <input type="submit" value="Registrieren">
+        </form>
+
+
+</x-base-layout>
