25353f7134c3336bb17a12d03aa4d2ddec6eeab8
Luke <luke.vincent.braun@gmail.com>
2022-12-12 21:29:55 +0100

    migration system-control

--- resources/views/system-control.blade.php
+++ resources/views/system-control.blade.php
@@ -1,18 +1,19 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+<x-base-layout :title="'System Controll'">
+
+    <nav>
+        <x-forms.back/>
+        <h1>System Controll</h1>
+    </nav>
+
+    <main class="center-content">
+
         <!--TODO(Link hinzufügen)-->
         <a href="{{ route('logout') }}"></a>
 
         <a href="{{route('question.index')}}">Fragenkatalog</a>
         <a href="{{route('category-add')}}">Kategorie hinzufügen</a>
         <a href="{{route('category.index')}}">Kategorie bearbeiten</a>
-</body>
-</html>
+
+    </main>
+
+</x-base-layout>
