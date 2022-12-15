61dfdb5560599ee88fc35d1e37b6668cd7a47afb
Manuel Claßen <79590230+Manuel3101@users.noreply.github.com>
2022-12-15 15:48:59 +0100

    Changed to new layout and translated an error message.

--- lang/de/auth.php
+++ lang/de/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
+    'failed' => 'Benutzername oder Passwort sind inkorrekt. Bitte überprüfen sie ihre Eingaben',
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,53 +1,51 @@
-<html lang="de">
-    <head>
-        <meta charset="UTF-8"/>
-        <title>Login</title>
-    </head>
-    <body>
-        <main>
+<x-base-layout :title="'Login'">
+    @section('navigation')
+        <nav>
+            <x-forms.back/>
+            <h1>Login</h1>
+        </nav>
+    @endsection
+    <div id="hub">
+        <div id="logo"></div>
+    </div>
+
+        <form method="POST" action="{{ route('login') }}" >
+            @csrf
+            <!-- Fehlermeldungen -->
+            <x-input-error :messages="$errors->all()"/>
+
+            <!-- Benutzername -->
             <div>
-                <form method="POST" action="{{ route('login') }}" >
-                    @csrf
-                    <!-- Fehlermeldungen -->
-                    @if(false)
-                        <div>
-                            <label>{{ __('Fehlermeldung') }}</label>
-                        </div>
-                    @endif
-
-                    <!-- Benutzername -->
-                    <div>
-                        <label for="name"></label>
-                        <input type="text" name="name" id="name" placeholder="Ihr Benutzername" required />
-                    </div>
-
-                    <!-- Passwort -->
-                    <div>
-                        <label for="password"></label>
-                        <input type="password" name="password" id="password" placeholder="Ihr Passwort" required />
-                    </div>
-
-                    <!-- Einloggen -->
-                    <div>
-                        <input type="submit" id="login" value="{{ __('Einloggen') }}"/>
-                    </div>
-
-                    <!-- Passwort vergessen -->
-                    <div>
-                        @if(Route::has('password.request'))
-                            <a href="{{ route('password.request') }}">{{ __('Passwort vergessen?') }}</a>
-                        @endif
-                    </div>
-
-                    <!-- Registrieren -->
-                    <div>
-                        @if(Route::has('register'))
-                            <label>{{ __('Noch nicht registriert?') }}</label><br>
-                            <a href="{{ route('register') }}">{{ __('Registrieren') }}</a><br>
-                        @endif
-                    </div>
-                </form>
+                <label for="name"></label>
+                <input type="text" name="name" id="name" placeholder="Ihr Benutzername" />
             </div>
-        </main>
-    </body>
-</html>
+
+            <!-- Passwort -->
+            <div>
+                <label for="password"></label>
+                <input type="password" name="password" id="password" placeholder="Ihr Passwort" />
+            </div>
+
+            <!-- Einloggen -->
+            <div>
+                <input type="submit" id="login" value="{{ __('Einloggen') }}"/>
+            </div>
+
+        </form>
+
+        <!-- Passwort vergessen -->
+        <div>
+            @if(Route::has('password.request'))
+                <a href="{{ route('password.request') }}">{{ __('Passwort vergessen?') }}</a>
+            @endif
+        </div>
+
+        <!-- Registrieren -->
+        <div>
+            @if(Route::has('register'))
+                <label>{{ __('Noch nicht registriert?') }}</label><br>
+                <a href="{{ route('register') }}">{{ __('Registrieren') }}</a><br>
+            @endif
+        </div>
+
+</x-base-layout>
