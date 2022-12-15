13ffad966ca419c972718de8602ceb65cf6352e4
Manuel Claßen <79590230+Manuel3101@users.noreply.github.com>
2022-12-15 15:38:55 +0100

    Updated to properly use the layout

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,34 +1,29 @@
-
 <x-base-layout :title="'Email Verifikation'">
     @section('navigation')
         <nav>
+            <x-forms.back/>
             <h1>Email Verifikation</h1>
         </nav>
     @endsection
     <div id="hub">
         <div id="logo"></div>
     </div>
-
+        <!-- Ist eigentlich unnötig, aber sicher ist sicher -->
+        <x-input-error :messages="$errors->all()"/>
         <div>
             {{ __('Danke fürs registrieren, bitte bestätigen sie den Link in der Email, welche wir soeben verschickt haben.') }}
         </div>
-        @if(session('status') == 'verfication-link-sent')
-            <div>
-                {{ __('Ein neuer Bestätigungslink wurde verschickt') }}
-            </div>
-        @endif
-        <div>
-            <form method="POST" action="{{ route('verification.send') }}">
-                @csrf
-                <div>
-                    <input type="button" value="{{ __('Link neu senden') }}">
-                </div>
-            </form>
-        </div>
-        <form method="POST" action="{{ route('logout') }}">
+        <form class="center-content" method="POST" action="{{ route('verification.send') }}">
             @csrf
-            <button type="submit">{{ __('Logout') }}</button>
+            <div>
+                <input type="submit" value="{{ __('Link neu senden') }}">
+            </div>
         </form>
-
+        <form class="center-content" method="POST" action="{{ route('logout') }}">
+            @csrf
+            <div>
+                <input type="submit" value="{{__('Logout')}}"/>
+            </div>
+        </form>
 </x-base-layout>
 
