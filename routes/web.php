fbd6964a6c52c5b2ac6497d02f7759496eaf79ea
alvwin <93882810+alvwin@users.noreply.github.com>
2022-12-17 14:07:42 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('music_test');
+    return view('welcome');
 });
 
 Route::get('/dashboard', function () {
