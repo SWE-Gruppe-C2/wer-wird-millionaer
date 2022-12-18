1da6e46c20c6d851038ba50a82e4debc32b9d424
Manuel Claßen <79590230+Manuel3101@users.noreply.github.com>
2022-12-18 15:59:05 +0100

    Updated view structure.

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,29 +1,38 @@
 <x-base-layout :title="'Email Verifikation'">
-    @section('navigation')
+
         <nav>
             <x-forms.back/>
             <h1>Email Verifikation</h1>
         </nav>
-    @endsection
-    <div id="hub">
-        <div id="logo"></div>
-    </div>
+
+
+    <main class="center-content">
+
+        <div id="hub">
+            <div id="logo"></div>
+        </div>
+
         <!-- Ist eigentlich unnötig, aber sicher ist sicher -->
         <x-input-error :messages="$errors->all()"/>
+
         <div>
             {{ __('Danke fürs registrieren, bitte bestätigen sie den Link in der Email, welche wir soeben verschickt haben.') }}
         </div>
+
         <form class="center-content" method="POST" action="{{ route('verification.send') }}">
             @csrf
             <div>
                 <input type="submit" value="{{ __('Link neu senden') }}">
             </div>
         </form>
+
         <form class="center-content" method="POST" action="{{ route('logout') }}">
             @csrf
             <div>
                 <input type="submit" value="{{__('Logout')}}"/>
             </div>
         </form>
+
+    </main>
 </x-base-layout>
 
