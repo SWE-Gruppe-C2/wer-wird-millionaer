d695907f372a5cf735208136e2672ebe9171ea34
Pascal Klaßen <p.klassen221199@gmail.com>
2022-12-20 22:24:18 +0100

    CSRF für Logout

--- resources/views/components/forms/logout.blade.php
+++ resources/views/components/forms/logout.blade.php
@@ -1,5 +1,6 @@
 <!-- TODO: Muss als POST geschickt werden, Layout muss angepasst werden -->
 <form method="POST" action="{{ route('logout') }}">
+    @csrf
     <a href="{{ route('logout') }}" id="logout" onclick="event.preventDefault();
                                                 this.closest('form').submit();">
         <img src="{{ asset('assets/img/logout.png') }}" alt="Abmelden">
