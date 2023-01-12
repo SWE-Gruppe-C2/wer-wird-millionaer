ddee95296facf02076d23e728f866808d492f0a8
TimoKouhl <timokouhl@gmail.com>
2023-01-12 18:02:37 +0100

    Leaderboard angepasst

--- resources/views/leaderboard.blade.php
+++ resources/views/leaderboard.blade.php
@@ -8,13 +8,21 @@
         <h1>TOP 10</h1>
         <table id="highscores">
             <tbody>
+			@php $i = 1 @endphp
             @foreach($games as $game)
                 <tr>
-                    <td> {{$loop->iteration.'.'. $game->user->name }} </td>
-                    <td> {{ number_format($game->stage?->price,0,'.','.').'€' }} </td>
-                    <td> {{ $game->time_needed . ' min' }} </td>
+                    <td>{{ $i++.'. '. $game->user->name }}</td>
+                    <td>{{ number_format($game->stage?->price, 0, '.', '.').'€' }}</td>
+                    <td>{{ sprintf('%02d:%02d:%02d', ...explode(':', $game->time_needed)) . ' min' }}</td>
                 </tr>
             @endforeach
+			@while($i <= 10)
+				<tr>
+					<td>{{ $i++ }}.</td>
+					<td></td>
+					<td></td>
+				</tr>
+			@endwhile
             </tbody>
         </table>
 
@@ -31,5 +39,11 @@
             </div>
         </div>
     </div>
-
+	<script>
+        window.addEventListener('DOMContentLoaded', () => {
+            initMusic(0);
+            openingMusic();
+            muteCheck();
+        })
+	</script>
 </x-base-layout>
